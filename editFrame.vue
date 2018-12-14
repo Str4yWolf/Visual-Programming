3ab4fa45407e8b701419380f971ec144aafa50a8
atori247 <45831253+atori247@users.noreply.github.com>
2018-12-14 22:30:15 +0100

    Add files via upload

---
+++ editFrame.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>Blocks Editor</div>
+    <br/>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  props: ['num'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
