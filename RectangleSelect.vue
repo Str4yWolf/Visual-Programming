3a7eb5f6c58ca6ce00388ea299a490ef91512fc0
atori247 <45831253+atori247@users.noreply.github.com>
2019-01-26 05:09:16 +0100

    import of dummy image

--- RectangleSelect.vue
+++ RectangleSelect.vue
@@ -7,7 +7,7 @@
         @mousemove.native="selectActive($event,1)" hidden>
       </div>
     <div class="preview-image">
-      <img src="../assets/tensei-logo.png" v-bind:class="{ active: isActive }">
+      <img src="../assets/image.jpg" v-bind:class="{ active: isActive }">
     </div>
     </div>
     <button id="btn1" @click="select()">Select</button>
